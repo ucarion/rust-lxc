49309fcf22475c8091ffa23c70ed442e7a1ff1d6
Ulysse Carion <ulysse@ulysse.io>
2015-06-21 17:25:28 -0700

    Add a version() function.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,17 @@ extern crate libc;
 
 pub mod ffi;
 
+use ffi::lxccontainer;
+
+use std::ffi::CStr;
+use std::str;
+
+pub fn version<'a>() -> &'a str {
+    let version = unsafe { CStr::from_ptr(lxccontainer::lxc_get_version()) };
+    str::from_utf8(version.to_bytes()).unwrap()
+}
+
 #[test]
 fn it_works() {
+    assert_eq!("1.1.2", version());
 }
